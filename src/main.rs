434829b9783de72c8508d408bd790f4f415c7079
Sunil Kumar <kumar2000s@gmail.com>
2018-05-31 00:44:49 +0530

    fibonacci program in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Please enter the number!");
+    let mut num = String::new();
+    io::stdin().read_line( &mut num)
+        .expect("please enter integer number");
+    let num: i32= num.trim().parse()
+        .expect("please enter integer number");
+    let  mut fibs: Vec<i32> = Vec::new();
+    fibs.push(0);
+    fibs.push(1);
+
+    let  mut i= 2;
+    //let  lastv: Option<&i32> = fibs.get(fibs.len()-1);
+    while i<num {
+       let  lastval:i32  = fibs.get(fibs.len()-1).unwrap()+ fibs.get(fibs.len()-2).unwrap();
+       //println!("{}",lastval);
+        fibs.push(lastval);
+        i = i+1;
+
+    }
+
+    println!("{:?}",fibs);
+
 }
